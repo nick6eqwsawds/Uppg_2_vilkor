88e10fc25b8949ec787f720395782588b971b235
otto hoffman <otto.hoffman@outlook.com>
2023-10-23 08:39:51 +0200

    uppgifter 2 vilkor del B

--- App.java
+++ App.java
@@ -4,6 +4,8 @@ public class App {
     public static void main(String[] args)throws Exception{
         int litetTal=10;
         int stortTal=0;
+        int tal1 = 10;
+        int tal2 = 20;
 
         if(litetTal<1000){
             stortTal+=1000;
@@ -14,6 +16,10 @@ public class App {
 
         System.out.println("litetTal = "+litetTal+",stortTal = "+stortTal);
         //Koden funkar inte pga variabeln "stortTal" är gjord inom en if sats och den dör efter }
+
+        if(tal2>tal1){
+            System.out.println(tal2+" är större än "+tal1);
+        }
     }
     
 }
